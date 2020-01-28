ea1b57d9fb5ec73f174ee18354509e9d6600b316
zhangganlin <zhanggl1@shanghaitech.edu.cn>
2020-01-28 00:54:49 -0800

    finisher all questions, test all passed

--- proj1-search/searchAgents.py
+++ proj1-search/searchAgents.py
@@ -518,7 +518,7 @@ class ClosestDotSearchAgent(SearchAgent):
         problem = AnyFoodSearchProblem(gameState)
 
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return search.aStarSearch(problem)
 
 class AnyFoodSearchProblem(PositionSearchProblem):
     """
@@ -554,7 +554,7 @@ class AnyFoodSearchProblem(PositionSearchProblem):
         x,y = state
 
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return self.food[x][y]
 
 def mazeDistance(point1, point2, gameState):
     """
